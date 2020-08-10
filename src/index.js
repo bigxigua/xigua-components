d337714f77a421f2fd924904babb073dfd2b3a63
bigxigua <1336945839@qq.com>
2020-08-10 21:51:30 +0800

    feat: 调整项目目录结构

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import Alert from '@common/alert';
+import AutoComplete from '@common/auto-complete';
+import Breadcrumb from '@common/breadcrumb';
+import Button from '@common/button';
+import Dropdown from '@common/dropdown';
+import Empty from '@common/empty';
+import Icon from '@common/icon';
+import Image from '@common/image';
+import Input from '@common/input';
+import List from '@common/list';
+import Loading from '@common/loading';
+import M404 from '@common/m-404';
+import Message from '@common/message';
+import Modal from '@common/modal';
+import Pagination from '@common/pagination';
+import Scene from '@common/scene';
+import Select from '@common/select';
+import Table from '@common/table';
+import Tag from '@common/tag';
+import Tooltip from '@common/tooltip';
+import DeleteConfirm from '@common/delete-confirm';
+
+console.log('===========1');
+
+export default {
+  Alert,
+  AutoComplete,
+  // Breadcrumb,
+  Button,
+  Dropdown,
+  Empty,
+  Icon,
+  Image,
+  Input,
+  List,
+  Loading,
+  M404,
+  Message,
+  Modal,
+  Pagination,
+  Scene,
+  Select,
+  Table,
+  Tag,
+  Tooltip,
+  DeleteConfirm
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 
 module.exports = function webpackConfig(compile) {
+  // console.log(compile);
+  compile.plugins.shift();
   compile.resolve.alias = {
     ...compile.resolve.alias,
     ...({
       '@common': path.resolve(__dirname, './components'),
-      '@util': path.resolve(__dirname, './util'),
-      '@hooks': path.resolve(__dirname, './hooks'),
+      '@util': path.resolve(__dirname, './components/_util'),
+      '@hooks': path.resolve(__dirname, './components/_hooks'),
     })
   }
   return compile;
